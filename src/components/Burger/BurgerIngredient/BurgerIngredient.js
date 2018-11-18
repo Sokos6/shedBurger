a2198703d0bb32833c553101c47b575413b08689
Will Sokolowski <will.sokolowski@gmail.com>
2018-11-18 15:19:03 -0700

    burger ingredient and prop types

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import classes from './BurgerIngredient.module.css';
 
-const burgerIngredient = (props) => {
-let ingredient = null;
+class BurgerIngredient extends Component {
+    render () {
+        let ingredient = null;
 
-switch (props.type) {
+switch (this.props.type) {
     case ('bread-bottom'):
         ingredient = <div className={classes.BreadBottom}></div>;
         break;
@@ -31,6 +33,12 @@ switch (props.type) {
     default: ingredient = null;
     }
     return ingredient;
+    }
+}
+
+BurgerIngredient.propTypes = {
+    type: PropTypes.string.isRequired
 };
 
-export default burgerIngredient;
+
+export default BurgerIngredient;
